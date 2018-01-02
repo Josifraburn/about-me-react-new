26e1b44bed57ea8e104bfe8e2a1b3a82ff54dba2
Josif Raburn <raburnjosif@gmail.com>
2018-01-01 18:10:21 -0700

    I added the layout to bio and the styles

--- src/Containers/Bio/BioContainer.js
+++ src/Containers/Bio/BioContainer.js
@@ -5,26 +5,37 @@ import './BioContianer.css'
 class BioContainer extends Component {
     render() {
         return(
-            <div className='bio-main'>
-                <div className='bio-nav'>
-                    <Link to='/' className='bio-link'>
-                        HOME
-                    </Link>
-                     
-                    <Link to='/projects' className='bio-link'>
-                        PROJECTS
-                    </Link>
-                    <Link to='/resume' className='bio-link'>
-                        RESUME
-                    </Link>
+            <div className='bio-container'>
+                <div className='bio-background'>
                 </div>
-                <h1 className='name'>
-                    Josif Raburn
-                </h1>
-                <p className='bio-content'>
-                    Yo this is stuff about me so listen to it because I am cool guy!
-                </p>
-            </div>
+                <div className='bio-layout'>
+                    <img src='OtherFiles/josifimage.jpg' className='bio-image' /> 
+                    <div className='bio-left'>   
+                        <div className='bio-nav'>
+                            <Link to='/' className='bio-link'>
+                                HOME
+                            </Link>
+                            
+                            <Link to='/projects' className='bio-link'>
+                                PROJECTS
+                            </Link>
+                            <Link to='/resume' className='bio-link'>
+                                RESUME
+                            </Link>
+                        </div>
+                        <div className='bio-main-stuff'>
+                            <div className='name-content'>
+                                <h1 className='bio-name'>
+                                    Josif Raburn
+                                </h1>
+                                <p className='bio-content'>
+                                Yo this is stuff about me so listen to it because I am cool guy!
+                                </p>
+                            </div>
+                        </div>
+                    </div> 
+                </div>   
+            </div>    
         )
     }
 }
