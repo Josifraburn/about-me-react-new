ba276ab5dd2936dad169f4ce84cc26d131cb4220
Josif Raburn <raburnjosif@gmail.com>
2018-01-02 19:07:47 -0700

    I added animation to bio image and nav

--- src/Containers/Bio/BioContainer.js
+++ src/Containers/Bio/BioContainer.js
@@ -3,15 +3,32 @@ import { Link } from 'react-router-dom';
 import './BioContianer.css'
 
 class BioContainer extends Component {
+    state = {
+        bioImage: 'bioImageHidden',
+        bioNav: 'bioNavHidden',
+    }
+
+
+    componentWillMount() {
+        setTimeout(
+            function() {
+                this.setState ({
+                    bioImage: 'bio-image',
+                    bioNav: 'bio-nav',
+                })
+            }.bind(this), 3000
+        )
+    }
+
     render() {
         return(
             <div className='bio-container'>
                 <div className='bio-background'>
                 </div>
                 <div className='bio-layout'>
-                    <img src='OtherFiles/josifimage.jpg' className='bio-image' /> 
+                    <img src='OtherFiles/josifimage.jpg' className={this.state.bioImage} /> 
                     <div className='bio-left'>   
-                        <div className='bio-nav'>
+                        <div className={this.state.bioNav}>
                             <Link to='/' className='bio-link'>
                                 HOME
                             </Link>
@@ -29,10 +46,10 @@ class BioContainer extends Component {
                                     Josif Raburn
                                 </h1>
                                 <h2 className='bio-content'>
-                                I AM A 20 SOMETHING WEB DEVELOPER/DESIGNER<br />
-                                I HAVE A BEAUTIFUL WIFE AND 2 COOL DOGS<br />
-                                I LOVE VIDEO GAMES, MOVIES, AND LONG WALKS ON THE BEACH<br />
-
+                                I AM A 20 SOMETHING WEB DEVELOPER/DESIGNER.<br />
+                                I HAVE A BEAUTIFUL WIFE AND 2 COOL DOGS.<br />
+                                I LOVE VIDEO GAMES, MOVIES, AND LONG WALKS ON THE BEACH.
+                                HIRE ME FOR A GOOD TIME.
                                 </h2>
                             </div>
                         </div>
