7f627a2bbcb84f6ced9bdf19b2edcf9fd0b5ea01
Josif Raburn <raburnjosif@gmail.com>
2018-01-04 22:20:53 -0700

    I added the styles to the projects.css page

--- src/Containers/ProjectsContainer.js
+++ src/Containers/ProjectsContainer.js
@@ -7,12 +7,18 @@ class ProjectsContainer extends Component{
         return(
             <div className='projects-main'>
                 <div className='project1-container'>
-                    <div className='project1'>
+                    <img src='OtherFiles/josifimage.jpg' className='project1-img'/>
+                    <div className='project1-background'>
+                    </div>
+                    <div className='projects-content1'>
                         <ProjectsSkeleton projectName='Portfolio Site' info='This is info about my project'  />
                     </div>
                 </div>
                 <div className='project2-container'>
-                    <div className='project2'>
+                    <img src='OtherFiles/josifandeverrete.jpg' className='project2-img' />
+                    <div className='project2-background'>
+                    </div>
+                    <div className='projects-content2'>
                         <ProjectsSkeleton projectName='About Me Using React' info='This is other info about my project' />
                     </div>
                 </div>
